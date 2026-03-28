19d85296d9cf9a0a484b74aa0ea1e05fc8296205
WebUnica Chile <javier.millar@gmail.com>
2026-03-28 16:11:55 -0300

    UI: Removed phantom grid row in TypesSection to fix huge white space gap

--- src/components/home/types-section.tsx
+++ src/components/home/types-section.tsx
@@ -85,7 +85,7 @@ export function TypesSection() {
         </div>
 
         <motion.div 
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 lg:grid-rows-2"
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8"
           variants={containerVariants}
           initial="hidden"
           whileInView="show"
