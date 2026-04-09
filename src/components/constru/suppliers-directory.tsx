a6248d0cc48d4f2a02b030965e8f520551ccd8ad
WebUnica Chile <javier.millar@gmail.com>
2026-04-09 12:53:49 -0400

    fix: import missing cn utility in suppliers-directory

--- src/components/constru/suppliers-directory.tsx
+++ src/components/constru/suppliers-directory.tsx
@@ -14,6 +14,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { REGIONES_CHILE } from "@/config/regions";
 import { slugifyRegion } from "@/lib/regions";
 import { motion, AnimatePresence } from "framer-motion";
+import { cn } from "@/lib/utils";
 
 interface Category {
   id: string;
